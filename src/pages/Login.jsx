11a3a621ae61581a76b50f74faac9038d6b2848e
George Santos <george.santos_ufmg@outlook.com>
2022-08-01 15:40:32 -0300

    Implementação da lógica para o botão de login estar habilitado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 
+const MIN_LENGTH_NUMBER = 3;
+
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      buttonDisable: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    if (target.value.length >= MIN_LENGTH_NUMBER) {
+      this.setState({
+        buttonDisable: false,
+      });
+    } else {
+      this.setState({
+        buttonDisable: true,
+      });
+    }
+  }
+
   render() {
+    const { buttonDisable } = this.state;
     return (
       <div data-testid="page-login">
-        <input type="text" name="login" id="login" data-testid="login-name-input" />
-        <button type="submit">Entrar</button>
+        <input
+          type="text"
+          name="login"
+          id="login"
+          data-testid="login-name-input"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="submit"
+          data-testid="login-submit-button"
+          disabled={ buttonDisable }
+          // onClick={  }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
