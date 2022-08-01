1e7575b158015a25fd471454a5b1839f7b0a8136
George Santos <george.santos_ufmg@outlook.com>
2022-08-01 15:01:36 -0300

    Criação do componente Profile e da rota para o componente profile

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Route exact path="/search" component={ Search } />
           <Route exact path="/album/:id" component={ Album } />
           <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
         </Switch>
       </BrowserRouter>
     );
