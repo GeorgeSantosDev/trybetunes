790e146a43b4db196ddd8572abba62d59e1ccc7b
George Santos <george.santos_ufmg@outlook.com>
2022-08-01 18:19:05 -0300

    Implementação dos Links de navegação

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -28,6 +29,9 @@ class Header extends Component {
     const { isLoading, userName } = this.state;
     return (
       <header data-testid="header-component">
+        <Link to="/search" data-testid="link-to-search"> Pesquisar </Link>
+        <Link to="/favorites" data-testid="link-to-favorites"> Favoritas </Link>
+        <Link to="/profile" data-testid="link-to-profile"> Perfil </Link>
         { isLoading ? <Loading /> : <p data-testid="header-user-name">{ userName }</p> }
       </header>
     );
