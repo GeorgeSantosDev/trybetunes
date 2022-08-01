cd9603385494cd1425eb949db73d1b45655ea643
George Santos <george.santos_ufmg@outlook.com>
2022-08-01 15:18:27 -0300

    Criação do botão de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,7 @@ class Login extends Component {
     return (
       <div data-testid="page-login">
         <input type="text" name="login" id="login" data-testid="login-name-input" />
+        <button type="submit">Entrar</button>
       </div>
     );
   }
