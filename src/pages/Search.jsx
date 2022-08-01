8eca4cf5287c700fa502bca78738ba3c9056fac6
George Santos <george.santos_ufmg@outlook.com>
2022-08-01 14:55:22 -0300

    Criação do componente Search e da rota para o componenete search

---
+++ src/pages/Search.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <div data-testid="page-search">
+        <p>teste</p>
+      </div>
+    );
+  }
+}
+
+export default Search;
