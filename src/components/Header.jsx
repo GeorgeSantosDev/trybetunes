945329e555518456ae8e250802605f7b8663d16c
George Santos <george.santos_ufmg@outlook.com>
2022-08-01 17:35:42 -0300

    Renderização de loading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from '../pages/Loading';
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      userName: '',
+    };
+  }
+
+  async componentDidMount() {
+    const object = await getUser();
+
+    this.setState({
+      userName: object.name,
+    }, () => {
+      this.setState({
+        isLoading: false,
+      });
+    });
+  }
+
   render() {
+    const { isLoading, userName } = this.state;
     return (
       <header data-testid="header-component">
-        <p>teste</p>
+        { isLoading ? <Loading /> : <p>{ userName }</p> }
       </header>
     );
   }
