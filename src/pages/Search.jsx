4245dc07c7ba972d70f6d28c7b92c08f5ea9feaa
George Santos <george.santos_ufmg@outlook.com>
2022-08-01 18:54:20 -0300

    Implementando a lógica de habilitar o botão

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 
+const MIN_CHAR_NUMBER = 2;
+
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isDisable: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    if (target.value.length >= MIN_CHAR_NUMBER) {
+      this.setState({
+        isDisable: false,
+      });
+    }
+  };
+
   render() {
+    const { isDisable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <input type="text" name="artist" id="arttist" data-testid="search-artist-input" />
-        <button type="submit" data-testid="search-artist-button">Pesquisar</button>
+        <input
+          type="text"
+          name="artist"
+          id="arttist"
+          data-testid="search-artist-input"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ isDisable }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
