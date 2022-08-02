9b011fc3d3fe5e143e70a4dbbe5fdeb9dd583124
George Santos <george.santos_ufmg@outlook.com>
2022-08-02 14:30:52 -0300

    Utilizando a função getMusics para realizar um requisição

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    getMusics(id);
+  }
+
   render() {
     return (
       <div data-testid="page-album">
@@ -10,5 +17,9 @@ class Album extends Component {
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.objectOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+};
 
 export default Album;
