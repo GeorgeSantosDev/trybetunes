07e80486ca94f9e0dec45cf56f28d879baf5380e
George Santos <george.santos_ufmg@outlook.com>
2022-08-03 18:08:45 -0300

    implementando a lógica para buscar as informações do usuário

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      Loading: false,
+    };
+  }
+
+  componentDidMount() {
+    getUser();
+  }
+
   render() {
+    const { loading } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        { loading && <Loading /> }
       </div>
     );
   }
