406b84fb9120109a2144434d5a3fade3434034f6
George Santos <george.santos_ufmg@outlook.com>
2022-08-03 17:08:51 -0300

    Reaproveitando a lógica de fetchFavoriteSongs implementada em albums para buscar as músicas favoritadas

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      favoritesSong: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchFavoriteSongs();
+  }
+
+  fetchFavoriteSongs = () => {
+    this.setState({ loading: true }, async () => {
+      const songsFavorited = await getFavoriteSongs();
+      this.setState({
+        favoritesSong: songsFavorited.map((obj) => obj.trackId),
+        loading: false,
+      });
+    });
+  }
+
   render() {
+    const { loading } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
+        { loading && <Loading /> }
       </div>
     );
   }
