e3d8311f7d2cbf2001303914b59f3417251bd37c
George Santos <george.santos_ufmg@outlook.com>
2022-08-03 19:27:23 -0300

    Rnederizando as informações do usuário na tela

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
@@ -8,20 +9,40 @@ class Profile extends Component {
     super();
 
     this.state = {
-      Loading: false,
+      loading: false,
+      userInfos: '',
     };
   }
 
   componentDidMount() {
-    getUser();
+    this.fetchUserInfos();
+  }
+
+  fetchUserInfos = () => {
+    this.setState({ loading: true }, async () => {
+      const getInfos = await getUser();
+      this.setState(({
+        loading: false,
+        userInfos: getInfos,
+      }));
+    });
   }
 
   render() {
-    const { loading } = this.state;
+    const { loading, userInfos } = this.state;
+    const { name, email, description, image } = userInfos;
     return (
       <div data-testid="page-profile">
         <Header />
         { loading && <Loading /> }
+        <p>Nome</p>
+        <p>{ name }</p>
+        <p>Email</p>
+        <p>{ email }</p>
+        <img src={ image } alt={ name } data-testid="profile-image" />
+        <p>Descrição</p>
+        <p>{ description }</p>
+        <Link to="/profile/edit">Editar perfil</Link>
       </div>
     );
   }
