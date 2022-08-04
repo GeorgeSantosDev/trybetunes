c5818f4b153904520889da2a02c767e01e211743
George Santos <george.santos_ufmg@outlook.com>
2022-08-04 09:16:15 -0300

    implementando a lógica para recuperar as informações dos usuários

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class ProfileEdit extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: '',
+      userInfos: '',
+    };
+  }
+
+  componentDidMount() {
+    this.fetchUserInfos();
+  }
+
+  fetchUserInfos = () => {
+    this.setState({ loading: true }, async () => {
+      const getInfos = await getUser();
+      this.setState(({
+        loading: false,
+        userInfos: getInfos,
+      }));
+    });
+  }
+
   render() {
+    const { loading, userInfos } = this.state;
     return (
       <div data-testid="page-profile-edit">
         <Header />
+        { loading && <Loading /> }
       </div>
     );
   }
