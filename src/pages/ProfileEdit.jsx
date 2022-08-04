04af58825e3ee1164afad98b950c88778ac66221
George Santos <george.santos_ufmg@outlook.com>
2022-08-04 09:25:56 -0300

    implementando a lógica para renderizar o formulário na tela

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -29,10 +29,43 @@ class ProfileEdit extends Component {
 
   render() {
     const { loading, userInfos } = this.state;
+    const { name, email, description, image } = userInfos;
     return (
       <div data-testid="page-profile-edit">
         <Header />
         { loading && <Loading /> }
+        <form>
+          <input
+            type="text"
+            name="name"
+            id="userName"
+            data-testid="edit-input-name"
+            value={ name }
+          />
+          <input
+            type="email"
+            name="email"
+            id="userEmail"
+            data-testid="edit-input-email"
+            value={ email }
+          />
+          <textarea
+            name="description"
+            id="userDescription"
+            cols="30"
+            rows="10"
+            data-testid="edit-input-description"
+            value={ description }
+          />
+          <input
+            type="text"
+            name="image"
+            id="userImage"
+            data-testid="edit-input-image"
+            value={ image }
+          />
+          <button type="submit" data-testid="edit-button-save">Salvar</button>
+        </form>
       </div>
     );
   }
