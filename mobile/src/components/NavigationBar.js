d171873aa092e22aff69725c82c7c2f199ebdd92
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-03-30 22:14:53 -0300

    feat: Create NavigationBar

---
+++ mobile/src/components/NavigationBar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View, Image, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import favoriteIcon from '../../images/favoriteIcon.png';
+import profileIcon from '../../images/profileIcon.png';
+import searchIcon from '../../images/searchIcon.png';
+
+const width = Dimensions.get('screen').width;
+
+function NavigationBar() {
+  const navigation = useNavigation();
+
+  const handlePress = (screen) => {
+    navigation.navigate(screen);
+  };
+
+  return (
+    <View style={ styles.container }>
+      <TouchableOpacity onPress={ () => handlePress('Home') }>
+        <Image
+          source={searchIcon}
+          style={ styles.icon }
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={ () => handlePress('Favorites') }>
+        <Image
+          source={favoriteIcon}
+          style={ styles.icon }
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={ () => handlePress('Profile') }>
+        <Image
+          source={profileIcon}
+          style={ styles.icon }
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#EFE9E1',
+    padding: 10,
+    width: width,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  icon: {
+    width: 20,
+    height: 20,
+  },
+});
+
+
+export default NavigationBar;
