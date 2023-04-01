3444ff86cb14bd97bc4386f8a74e5cd748df0216
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-04-01 14:07:14 -0300

    refactor: Change Text element to Button element

--- mobile/src/components/AlbumCard.js
+++ mobile/src/components/AlbumCard.js
@@ -3,19 +3,36 @@ import { View,
   Image,
   Text,
   StyleSheet,
-  Dimensions } from 'react-native';
+  Dimensions,
+  Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const width = Dimensions.get('screen').width;
 
 function AlbumCard({ album }) {
+  const navigation = useNavigation();
+
+  const onPress = () => {
+    navigation.navigate('Album', { albumId: album.collectionId });
+  };
+
   return (
     <View style={ styles.container }>
-      <Image
-        source={{ uri: album.artworkUrl100 }}
-        style={ styles.image }
-      />
+      <View style={ styles.shadowContainer }>
+        <Image
+          source={{ uri: album.artworkUrl100 }}
+          style={ styles.image }
+        />
+      </View>
       <Text style={ styles.text }>{ album.collectionName }</Text>
-      <Text style={ styles.text }>Acessar - Será um botão</Text>
+      <View style={ styles.buttonContainer }>
+        <Button
+          onPress={ onPress }
+          title="Acessar"
+          backgroundColor="#003BE5"
+          accessibilityLabel="Access button"
+        />
+      </View>
     </View>
   );
 }
@@ -24,7 +41,7 @@ const styles = StyleSheet.create({
   container: {
     alignSelf: 'center',
     width: width * 0.8,
-    height: width * 0.6,
+    height: width * 0.8,
     borderRadius: 10,
     marginTop: 25,
     marginBottom: 40,
@@ -37,6 +54,18 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+  },
+  buttonContainer: {
+    width: width * 0.3,
+    alignSelf: 'center',
+    marginTop: 20,
+  },
+  shadowContainer: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
